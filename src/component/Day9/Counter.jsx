b5027bc81976fef1bd309eec32e06035c696e35d
Aruna501 <153171890+Aruna501@users.noreply.github.com>
2024-03-27 13:58:06 +0530

    Add files via upload

---
+++ src/component/Day9/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+function Counter() {
+
+    const [value, setValue] = useState(5)
+    const [count, setCount] = useState("")
+
+    const handleChange = (e)=>{
+      setCount(parseInt(e.target.value));
+    }
+
+    const handleIncreament = () => {
+      setValue(value + count)
+    }
+
+    const handleDecreament = () => {
+      setValue(value - count)
+    }
+
+  return (
+    <div>
+        <h3>Count: {value} <span><input type="number" name="count" value={count} onChange={handleChange}/></span></h3>
+       <button onClick={handleIncreament}>Increament</button>
+       <button onClick={handleDecreament}>Decreament</button>
+    </div>
+  )
+}
+
+export default Counter
