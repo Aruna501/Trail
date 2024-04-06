383f9c3be100a57d550302123d4652755d675eb9
Aswini <aswini.j@doodleblue.com>
2024-04-06 19:19:46 +0530

    using this one hover and active function purpose

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route} from 'react-router-dom'
-
-import Home from './Home';
-import About from './About';
-import Profile from './Profile';
-import Contact from './Contact';
-import Projects from './Projects';
-import Navbar from './Navbar';
-import ErrorPage from './ErrorPage';
-import Welcomemessage from './Welcomemessage';
-
-
+import React from 'react'
+import Navbar from './Navbar'
 
 function App() {
-
   return (
-
-    <div className="nav-main-page">
-    <BrowserRouter>
-                <Navbar />
-      <Switch>
-      <Route exact path="/" render={(props) => (
-      <> <Home /> <Welcomemessage componentName="Home" /> </> )
-      } />
-        <Route path="/about" render={(props) => ( <>
-        <About /> <Welcomemessage componentName="About" /> </>
-        )} />
-        <Route path="/profile" render={(props) => ( <>
-        <Profile /> <Welcomemessage componentName="Profile" /> </>
-        )} />
-        <Route path="/contact" render={(props) => ( <>
-        <Contact /> <Welcomemessage componentName="Contact" /> </>
-        )} />
-        <Route path="/projects" render={(props) => ( <>
-        <Projects /> <Welcomemessage componentName="Projects" /> </>
-        )} />
-          <Route path='*'>
-          <ErrorPage />
-        </Route>
-
-      </Switch>
-          </BrowserRouter>
-          </div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
